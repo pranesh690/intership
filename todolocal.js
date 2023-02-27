68d70f0fbd45831ef3c04d506eb376ba2cfe84c7
pranesh690 <82379183+pranesh690@users.noreply.github.com>
2023-02-27 12:12:40 +0545

    Add files via upload

---
+++ todolocal.js
@@ -0,0 +1,24 @@
+let addToDoButton = document.getElementById('addTodo');
+let todoBox = document.getElementById('todoBox');
+let inputField = document.getElementById('inputPlace');
+
+const removeTodo = (todo) => {
+  todoBox.removeChild(todo);
+  localStorage.setItem('todoList', todoBox.innerHTML);
+};
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText = inputField.value;
+    todoBox.appendChild(paragraph);
+    inputField.value="";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration="line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        removeTodo(paragraph);
+    });
+    localStorage.setItem('todoList', todoBox.innerHTML);
+});
+
+todoBox.innerHTML = localStorage.getItem('todoList');
